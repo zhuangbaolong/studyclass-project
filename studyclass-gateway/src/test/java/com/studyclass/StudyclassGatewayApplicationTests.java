d83fe54ef5072e58b90b3bedd3e72b1b12a118b3
zhuangbaolong <3294402246@qq.com>
2024-11-06 23:35:16 +0800

    微服务注册和网关

---
+++ studyclass-gateway/src/test/java/com/studyclass/StudyclassGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package com.studyclass;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyclassGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
--- studyclass-plus-system/studyclass-plus-system-api/src/main/java/com/studyclass/system/SystemApplication.java
+++ studyclass-plus-system/studyclass-plus-system-api/src/main/java/com/studyclass/system/SystemApplication.java
@@ -3,6 +3,7 @@ package com.studyclass.system;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 @EnableSwagger2Doc
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SystemApplication {
 
     public static void main(String[] args) {
