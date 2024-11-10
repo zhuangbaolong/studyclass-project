314f0e04c64b19e6780b2e8901a4e6aa5e7eaff5
zhuangbaolong <3294402246@qq.com>
2024-11-10 16:17:05 +0800

    jwt令牌访问内容管理资源服务

--- studyclass-content/studyclass-content-api/src/main/java/com/studyclass/content/api/CourseBaseInfoController.java
+++ studyclass-content/studyclass-content-api/src/main/java/com/studyclass/content/api/CourseBaseInfoController.java
@@ -6,9 +6,13 @@ import com.studyclass.base.model.PageResult;
 import com.studyclass.content.model.dto.*;
 import com.studyclass.content.model.po.CourseBase;
 import com.studyclass.content.service.CourseBaseInfoService;
+import com.studyclass.content.util.SecurityUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +31,7 @@ public class CourseBaseInfoController {
     CourseBaseInfoService courseBaseInfoService;
 
     @ApiOperation("课程查询接口")
+    @PreAuthorize("hasAuthority('xc_teachmanager_course_list')")
     @PostMapping("/course/list")
     public PageResult<CourseBase> list(PageParams pageParams, @RequestBody(required = false) QueryCourseParamsDto queryCourseParams) {
         PageResult<CourseBase> pageResult = courseBaseInfoService.queryCourseBaseList(pageParams, queryCourseParams);
@@ -43,6 +48,11 @@ public class CourseBaseInfoController {
     @ApiOperation("查看课程")
     @GetMapping("/course/{courseId}")
     public CourseBaseInfoDto getCourseBaseInfoById(@PathVariable Long courseId){
+        //获取当前身份信息json串
+//        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        System.out.println(principal);
+        SecurityUtil.XcUser user = SecurityUtil.getUser();
+        System.out.println(user.getUsername());
         return courseBaseInfoService.getCourseBaseInfo(courseId);
     }
 
